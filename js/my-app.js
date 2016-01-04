e12a357b8e9b93cfe2910a6c3eee0b440d9f2a3b
lid <lid@1.com>
2016-01-04 18:16:34 +0800

    none

--- js/my-app.js
+++ js/my-app.js
@@ -9,6 +9,7 @@ var mainView = myApp.addView('.view-main', {
     //动态导航栏：只被ios支持，它只对穿透类型的布局有效
     dynamicNavbar: true,
     //animateNavBackIcon额外的动画，以使这些链接看起来更像iOS 7外观
-    animateNavBackIcon: true
+    animateNavBackIcon: true,
+    swipePanel: 'left'
 });
 
